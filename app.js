d7ae82e92600957f630b642c875345c6599f80c5
Sergej Pavlenko <darksamurai124@gmail.com>
2022-12-17 00:18:21 +0000

    Add app logic

---
+++ app.js
@@ -0,0 +1,3 @@
+const cool = () => {
+	console.log("Function call from app.js");
+};
--- index.js
+++ index.js
@@ -1,6 +1,10 @@
+from app import cool
+
 let i = 0;
 for (i = 0; i < 20; i++)
 {
 	console.log(i);
 }
 alert("Hello");
+
+cool();
